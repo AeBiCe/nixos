dbeb5d2b68d64fafc0c54db81da33770a95e8fc6
AeBiCe <info@aebice.net>
2025-03-04 23:15:39 +0100

    Configured Nvidia drivers

--- configuration.nix
+++ configuration.nix
@@ -54,6 +54,11 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  #Nvidia Settings
+  hardware.graphics.enable = true;
+  hardware.nvidia.open = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
