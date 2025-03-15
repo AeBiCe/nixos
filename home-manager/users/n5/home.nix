67dd8596e3a3d8152d1c21d80bb6f8dcd00f0580
AeBiCe <info@aebice.net>
2025-03-15 21:51:09 +0100

    Setup central management of dotfiles via /dotfiles

--- home-manager/users/n5/home.nix
+++ home-manager/users/n5/home.nix
@@ -84,9 +84,13 @@
     # '')
   ];
 
+  home.file."${config.xdg.configHome}" = {
+     source = ../../../dotfiles;
+     recursive = true;
+  };
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  home.file = {
+  #home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
@@ -97,7 +101,7 @@
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
-  };
+  #};
 
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
