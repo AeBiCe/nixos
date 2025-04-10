a2465ac7846b4640aa0a0ef4e6a6ba96090ebf6b
AeBiCe <info@aebice.net>
2025-04-10 22:23:57 +0200

    Added vial for customizing my keyboard

--- modules/peripherals/default.nix
+++ modules/peripherals/default.nix
@@ -1,4 +1,17 @@
+{ pkgs, ... }:
+
 {
   #Enable Logitech wireless protocol
   hardware.logitech.wireless.enable = true;
+
+  environment.systemPackages = [ pkgs.vial ];
+
+  services.udev = {
+
+    packages = with pkgs; [
+      qmk-udev-rules # the only relevant
+      vial
+    ]; # packages
+
+  }; # udev
 }
