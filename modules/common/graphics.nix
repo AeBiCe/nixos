516dd6ccb3dcb013fe976a2fc7ace620a50e4ebc
AeBiCe <info@aebice.net>
2025-05-18 20:57:44 +0200

    Configured prime with offload setting for graphics

--- modules/common/graphics.nix
+++ modules/common/graphics.nix
@@ -3,4 +3,11 @@
   hardware.graphics.enable = true;
   hardware.nvidia.open = true;
   services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia.prime = {
+    offload.enable = true;
+    nvidiaBusId = "PCI:1:0:0";
+    amdgpuBusId = "PCI:73:0:0"; # AMD iGPU
+  };
+
 }
